2db2cf36282572d477bdc48f84921710cad531c4
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2018-05-04 10:51:04 +0700

    re-organized sql folder

--- db/db.sql
+++ db/db.sql
@@ -169,7 +169,7 @@ CREATE TABLE `db_online_course`.`Course`
  `CreateDate`  DATETIME DEFAULT CURRENT_TIMESTAMP() ,
  `Teacher_ID`  BIGINT NOT NULL ,
  `OpenedClass` BIGINT DEFAULT 0 ,
- `ImageLink`   VARCHAR(100) DEFAULT "http://www.raisedeyebrow.com/sites/www.raisedeyebrow.com/files/blog/2012/01/fff.png" ,
+ `ImageLink`   VARCHAR(128) DEFAULT "http://www.raisedeyebrow.com/sites/www.raisedeyebrow.com/files/blog/2012/01/fff.png" ,
 
 PRIMARY KEY (`Course_ID`),
 KEY `fkIdx_678` (`Teacher_ID`),
--- db/load.sh
+++ db/load.sh
@@ -4,13 +4,15 @@ echo 'Load schema'
 mysql -u $1 -p$2 < db.sql
 
 echo 'Load sample data'
-mysql -u $1 -p$2 db_online_course < sample.sql
+mysql -u $1 -p$2 db_online_course < sample/user-small.sql
+mysql -u $1 -p$2 db_online_course < sample/course-small.sql
 
 echo 'Load functions'
 mysql -u $1 -p$2 db_online_course < func.sql
 
 echo 'Load procedures'
-mysql -u $1 -p$2 db_online_course < proc.sql
+mysql -u $1 -p$2 db_online_course < proc/user.sql
+mysql -u $1 -p$2 db_online_course < proc/course.sql
 
 echo 'Load triggers'
 mysql -u $1 -p$2 db_online_course < trigger.sql
